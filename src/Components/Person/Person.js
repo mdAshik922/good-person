08984cddf476aaef4804ae42467c4c82f2bc8f27
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-03 00:08:44 +0600

    commit

--- src/Components/Person/Person.js
+++ src/Components/Person/Person.js
@@ -1,23 +1,22 @@
 import React from 'react';
-
 import './Person.css';
 
 const Person = (props) => {
-    //destructring use
+   
 const {name, picture, age, company, balance, address, phone}= props.person;
 
     return (
         //dynamic 
         <div>
-            <h2>Name:{name}</h2>
+            
             <img src={picture} alt=''/>
-       
+            <h2>Name:{name}</h2>
 <h3>Company:{company}</h3>
 <p>age:{age}</p>
 <h5>Salary:${balance}</h5>
 <h4>Phone Number:{phone}</h4>
 <p>Address:{address}</p>
-<button onClick={() =>
+<button style={{marginBottom: "5%"}} onClick={() =>
  props.payThisPerson(props.person)} className='payThisPerson'
 >
     Pay</button>
